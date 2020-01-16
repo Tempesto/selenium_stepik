ae34d35825692e6961946fdfc7ae7a0923bb2ae7
Tempesto <igor_it_step@gmail.com>
2020-01-16 11:39:47 +0200

    add test_lesson3_step2.13.py del time and add webdriverwait

--- test_lesson3_step2.13.py
+++ test_lesson3_step2.13.py
@@ -1,63 +1,39 @@
 import unittest
 from conf import browser, ID, TAG, GET
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 class TestRegistration(unittest.TestCase):
     def test_1(self):
         GET("http://suninjuly.github.io/registration1.html")
-
-
-        # Ваш код, который заполняет обязательные поля
         input1 = TAG("[placeholder='Input your first name']")
         input1.send_keys("Ivan")
         input2 = TAG("[placeholder='Input your last name']")
         input2.send_keys("Sokolov")
         input3 = TAG("[placeholder='Input your email']")
         input3.send_keys("sos@ya.ru")
-
-        # Отправляем заполненную форму
         button = browser.find_element_by_css_selector("button.btn")
         button.click()
-
-        # Проверяем, что смогли зарегистрироваться
-        # ждем загрузки страницы
-        time.sleep(1)
-
-        # находим элемент, содержащий текст
+        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "h1")))
         welcome_text_elt = TAG("h1")
-        # записываем в переменную welcome_text текст из элемента welcome_text_elt
         welcome_text = welcome_text_elt.text
-
-        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
         self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Тест '1' не прошел")
 
     def test_2(self):
         GET("http://suninjuly.github.io/registration2.html")
-
-
-        # Ваш код, который заполняет обязательные поля
         input1 = TAG("[placeholder='Input your first name']")
         input1.send_keys("Ivan")
         input2 = TAG("[placeholder='Input your last name']")
         input2.send_keys("Sokolov")
         input3 = TAG("[placeholder='Input your email']")
         input3.send_keys("sos@ya.ru")
-
-        # Отправляем заполненную форму
         button = browser.find_element_by_css_selector("button.btn")
         button.click()
-
-        # Проверяем, что смогли зарегистрироваться
-        # ждем загрузки страницы
-        time.sleep(1)
-
-        # находим элемент, содержащий текст
+        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "h1")))
         welcome_text_elt = browser.find_element_by_tag_name("h1")
-        # записываем в переменную welcome_text текст из элемента welcome_text_elt
         welcome_text = welcome_text_elt.text
-
-        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
         self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Тест '2' не прошел")
 
 
