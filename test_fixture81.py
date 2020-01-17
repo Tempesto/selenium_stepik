26cfaa6254019109e306f79403a0bb6fbb77f3ef
Tempesto <igor_it_step@gmail.com>
2020-01-17 15:50:57 +0200

    add test_fixture81.py test mark for win10 and add to pytest.ini mark win10 and commit text

--- test_fixture81.py
+++ test_fixture81.py
@@ -25,3 +25,8 @@ class TestMainPage1(object):
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+
+# Чтобы запустить только smoke-тесты для Windows 10, нужно использовать логическое И:
+#
+# pytest -s -v -m "smoke and win10" test_fixture81.py
